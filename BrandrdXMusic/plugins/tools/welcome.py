3f4ccf926ea8fcf31cd031001821c2f6479aa963
Vipin Kumar <vipin275869@gmail.com>
2025-05-15 11:39:24 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -91,7 +91,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
 ๏ 𝗜𝗗 ➠ {user.id}
 ๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [ㅤᎪɴᴜʀᴀɢㅤㅤ](https://t.me/ANURAG_ABOUT)
+๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [ㅤᎪɴᴜʀᴀɢㅤㅤ](https://t.me/Vip_chor)
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/ANURAG_MUSIC_1_BOT?startgroup=true")]])
