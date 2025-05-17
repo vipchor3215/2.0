814c24afecf1a42011e4addcfa75b6ee7e377f84
Vipin Kumar <vipin275869@gmail.com>
2025-05-17 15:44:32 +0530

    Update image.py

--- BrandrdXMusic/plugins/tools/image.py
+++ BrandrdXMusic/plugins/tools/image.py
@@ -33,12 +33,12 @@ async def google_img_search(client: Client, message: Message):
     except Exception as e:
         return await message.reply(f"Error in downloading images: {e}")
 
-    msg = await message.reply("ㅤᎪɴᴜʀᴀɢㅤㅤ Scrapping images...")
+    msg = await message.reply("𝗗꯭ɪʟ꯭c፝֟͠ʜ꯭𝛔ʀ꯭ Scrapping images...")
 
     count = 0
     for img in lst:
         count += 1
-        await msg.edit(f"=> ㅤᎪɴᴜʀᴀɢㅤㅤ owo scrapped images {count}")
+        await msg.edit(f"=> 𝗗꯭ɪʟ꯭c፝֟͠ʜ꯭𝛔ʀ꯭ owo scrapped images {count}")
 
     try:
         await app.send_media_group(
