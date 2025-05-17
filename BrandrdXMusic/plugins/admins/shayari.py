7ac8ced280eb4c2ae3708dbbc3a693da4898a1e4
Vipin Kumar <vipin275869@gmail.com>
2025-05-17 15:24:03 +0530

    Update shayari.py

--- BrandrdXMusic/plugins/admins/shayari.py
+++ BrandrdXMusic/plugins/admins/shayari.py
@@ -177,4 +177,4 @@ async def cancel_spam(client, message):
             spam_chats.remove(message.chat.id)
         except:
             pass
-        return await message.reply("👣 ANURAG 𝐒𝐇𝐀𝐘𝐀𝐑𝐈 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 💗")
+        return await message.reply("👣 𝗗꯭ɪʟ꯭c፝֟͠ʜ꯭𝛔ʀ꯭ 𝐒𝐇𝐀𝐘𝐀𝐑𝐈 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 💗")
