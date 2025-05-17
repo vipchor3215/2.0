7fefae27705f73851cdcf55ed4a46e6915e90298
Vipin Kumar <vipin275869@gmail.com>
2025-05-17 14:36:47 +0530

    Update help.py

--- BrandrdXMusic/utils/inline/help.py
+++ BrandrdXMusic/utils/inline/help.py
@@ -7,7 +7,7 @@ from BrandrdXMusic import app
     
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
+    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"🍬 𝐂𝐋𝐎𝐒𝐄 🍬")]
     second = [
         InlineKeyboardButton(
             text=_["BACK_PAGE"],
